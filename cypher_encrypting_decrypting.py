a86583d5a1c07d681f30bb60fc6418a6e83ea45b
Yair mizrachi <80672028+yair89999@users.noreply.github.com>
2023-03-06 19:36:52 +0200

    Add files via upload

---
+++ cypher_encrypting_decrypting.py
@@ -0,0 +1,38 @@
+import random
+
+
+characters_string = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890~!@#$%^&*()_+=-[]{}'\|<>,./?`"
+characters_list = []
+for s in characters_string:
+    characters_list.append(s)
+
+
+to_encrypt = input("Enter the string you want to encrypt:")
+def encrypt(to_encrypt:str):
+    key = random.randint(0,(len(characters_list)-1)*2)**5 # to make it harder to breach
+    encrypted = ""
+    for s in to_encrypt:
+        i = characters_list.index(s)
+        i += key
+        i = i % (len(characters_list)-1)
+        encrypted += characters_list[i]
+    return encrypted,key
+encrypted,key = encrypt(to_encrypt)
+print("not encrypted: " + to_encrypt)
+print("encrypted:     " + encrypted)
+print("to decrypt use this key:",key)
+
+def decrypt(to_decrypt:str,key:int):
+    decrypted = ""
+    for s in to_decrypt:
+        i = characters_list.index(s)
+        i -= key
+        i = i % (len(characters_list)-1)
+        i = abs(i)
+        decrypted += characters_list[i]
+    return decrypted
+
+decrypted = decrypt(encrypted,key)
+print("encrypted: " + encrypted)
+print("decrypt:   " + decrypted)
+print("key used:",key)
